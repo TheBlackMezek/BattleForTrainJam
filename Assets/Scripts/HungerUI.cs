ecdf831d34519a0717756bb4b03cb667e98c8974
Alexander Thomas <6ftDwarf@gmail.com>
2018-12-18 13:00:47 -0800

    Add art assets

--- Assets/Scripts/HungerUI.cs
+++ Assets/Scripts/HungerUI.cs
@@ -9,12 +9,17 @@ public class HungerUI : MonoBehaviour {
     private Hunger hunger;
     [SerializeField]
     private RectTransform bar;
+    [SerializeField]
+    private Image hungerImage;
 
 
 
     private void Update()
     {
-        bar.localScale = new Vector3(hunger.hunger / hunger.MaxHunger, 1f, 1f);
+        float hungerLevel = hunger.hunger / hunger.MaxHunger;
+        float imgTransparency = hungerLevel > 0.5f ? 0f : 1f - hungerLevel * 2f;
+        bar.localScale = new Vector3(hungerLevel, 1f, 1f);
+        hungerImage.color = new Color(hungerImage.color.r, hungerImage.color.g, hungerImage.color.b, imgTransparency);
     }
 
 }
